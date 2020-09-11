6d7c6d9213ee0ce74632e7de6737bda6c020f0e6
Ryan Bird <rlbird22@gmail.com>
2020-09-10 20:18:41 -0600

    Alter python script

--- module2/python3_multithreading_example/example.py
+++ module2/python3_multithreading_example/example.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import _thread
+import random
+import sys
 import time
 
 # Define a function for the thread
@@ -12,9 +14,18 @@ def print_time( threadName, delay):
       print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
 
 # Create two threads as follows
+num_threads = 2
+
+if len(sys.argv):
+    num_threads = int(sys.argv[0])
+
 try:
-   _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
-   _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+    for i in Range(num_threads):
+       delay = random.randint(1, 4)
+       _thread.start_new_thread( print_time, (f"Thread-{num_threads + 1}", delay, ) )
+
+#       _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+#       _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
 except:
    print ("Error: unable to start thread")
 
