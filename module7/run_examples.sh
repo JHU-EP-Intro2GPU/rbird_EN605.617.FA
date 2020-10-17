23841feba2ee48f1d4d9f70c297c33e19d51b080
Ryan Bird <rlbird22@gmail.com>
2020-10-16 22:40:09 -0600

    Add examples bash script

---
+++ module7/run_examples.sh
@@ -0,0 +1,9 @@
+
+
+./assignment.exe --elements 100000000 --streamSize 500000 --streams 1 --blocksize 256 --disablePopulateData
+./assignment.exe --elements 100000000 --streamSize 500000 --streams 2 --blocksize 256 --disablePopulateData
+./assignment.exe --elements 100000000 --streamSize 500000 --streams 3 --blocksize 256 --disablePopulateData
+./assignment.exe --elements 100000000 --streamSize 500000 --streams 4 --blocksize 256 --disablePopulateData
+
+
+./assignment.exe --elements 100000000 --streamSize 500000 --streams 4 --blocksize 256
