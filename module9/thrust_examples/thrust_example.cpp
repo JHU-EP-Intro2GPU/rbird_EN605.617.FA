754b2e0c7c4f5a2a99cb55069d4a90133e0eec81
student <rbird5@jhu.edu>
2020-10-27 20:11:24 -0700

    thrust example changes

--- module9/thrust_examples/thrust_example.cpp
+++ module9/thrust_examples/thrust_example.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+#define print(cuda_vector)\
+  for(int i = 0; i < cuda_vector.size(); i++)\
+  {\
+    std::cout << #cuda_vector <<"[" << i << "] = " << cuda_vector[i] << std::endl;\
+  }
+
+
 int main(void)
 {
   // H has storage for 4 integers
@@ -18,28 +25,34 @@ int main(void)
   std::cout << "H has size " << H.size() << std::endl;
 
   // print contents of H
-  for(int i = 0; i < H.size(); i++)
-  {
-    std::cout << "H[" << i << "] = " << H[i] << std::endl;
-  }
+    print(H);
 
   // resize H
   H.resize(2);
 
   std::cout << "H now has size " << H.size() << std::endl;
+    
+    print(H);
+    
+   std::cout << std::endl;
 
-  // Copy host_vector H to device_vector D
+  // Copy host_vector H to device_vector D1 (there appears to be a delay on the first
+    // call to the device, likely setting up the cuda context)
+    std::cout << "Copying to device (D0)" << std::endl;
+  thrust::device_vector<int> D0 = H;
+
+    std::cout << "Copying to device (D)" << std::endl;
   thrust::device_vector<int> D = H;
+    
+    print(D);
 
   // elements of D can be modified
+    std::cout << "Direct device manipulation" << std::endl;
   D[0] = 99;
   D[1] = 88;
 
   // print contents of D
-  for(int i = 0; i < D.size(); i++)
-  {
-    std::cout << "D[" << i << "] = " << D[i] << std::endl;
-  }
+    print(D);
 
   // H and D are automatically destroyed when the function returns
   return 0;
