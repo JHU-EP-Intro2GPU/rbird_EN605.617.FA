fb13a4a650b7cd44a6d473319b02215442348a94
student <rbird5@jhu.edu>
2020-10-29 21:05:53 -0700

    Refactor nvGraph to be more readable with c++ containers

--- module9/nvgraph assignment/nvGraph_assignment.cu
+++ module9/nvgraph assignment/nvGraph_assignment.cu
@@ -7,7 +7,9 @@
 // starting point:
 // Nvidia example, https://docs.nvidia.com/cuda/nvgraph/index.html#nvgraph-sssp-example
 
+#include <memory>
 #include <stdio.h>
+#include <vector>
 
 void check(nvgraphStatus_t status) {
     if (status != NVGRAPH_STATUS_SUCCESS) {
@@ -16,46 +18,66 @@ void check(nvgraphStatus_t status) {
     }
 }
 
-int main()
+struct GraphData
 {
-    const size_t  n = 6, nnz = 10, vertex_numsets = 1, edge_numsets = 1;
-    float* sssp_1_h;
-    void** vertex_dim;
+    // sample data converted to stl containers
+    std::vector<float> weights_h = { 0.333333, 0.5, 0.333333, 0.5, 0.5, 1.0, 0.333333, 0.5, 0.5, 0.5 };
+    std::vector<int> destination_offsets_h = { 0, 1, 3, 4, 6, 8, 10 };
+    std::vector<int> source_indices_h = { 2, 0, 2, 0, 4, 5, 2, 3, 3, 4 };
+    
+    size_t numNodes() const { return destination_offsets_h.size() - 1; } // the final index is the number of edges
+    size_t numEdges() const { return source_indices_h.size(); }
+};
+
+int main()
+{    
     // nvgraph variables
-    nvgraphStatus_t status; nvgraphHandle_t handle;
+    nvgraphHandle_t handle;
     nvgraphGraphDescr_t graph;
-    nvgraphCSCTopology32I_t CSC_input;
     cudaDataType_t edge_dimT = CUDA_R_32F;
-    cudaDataType_t* vertex_dimT;
+    
+    GraphData graphData;
+    
     // Init host data
-    sssp_1_h = (float*)malloc(n * sizeof(float));
-    vertex_dim = (void**)malloc(vertex_numsets * sizeof(void*));
-    vertex_dimT = (cudaDataType_t*)malloc(vertex_numsets * sizeof(cudaDataType_t));
-    CSC_input = (nvgraphCSCTopology32I_t)malloc(sizeof(struct nvgraphCSCTopology32I_st));
-    vertex_dim[0] = (void*)sssp_1_h; vertex_dimT[0] = CUDA_R_32F;
-    float weights_h[] = { 0.333333, 0.5, 0.333333, 0.5, 0.5, 1.0, 0.333333, 0.5, 0.5, 0.5 };
-    int destination_offsets_h[] = { 0, 1, 3, 4, 6, 8, 10 };
-    int source_indices_h[] = { 2, 0, 2, 0, 4, 5, 2, 3, 3, 4 };
+    std::vector<float> sssp_1_h(graphData.numNodes());
+    
+    std::vector<void*> vertex_dim = { (void*)sssp_1_h.data() };
+    std::vector<cudaDataType_t> vertex_dimT = { CUDA_R_32F };
+    
+    nvgraphCSCTopology32I_t CSC_input = (nvgraphCSCTopology32I_t)malloc(sizeof(struct nvgraphCSCTopology32I_st));
+    
+    
     check(nvgraphCreate(&handle));
     check(nvgraphCreateGraphDescr(handle, &graph));
-    CSC_input->nvertices = n; CSC_input->nedges = nnz;
-    CSC_input->destination_offsets = destination_offsets_h;
-    CSC_input->source_indices = source_indices_h;
+    CSC_input->nvertices = graphData.numNodes();
+    CSC_input->nedges = graphData.numEdges();
+    CSC_input->destination_offsets = graphData.destination_offsets_h.data();
+    CSC_input->source_indices = graphData.source_indices_h.data();
+    
     // Set graph connectivity and properties (tranfers)
     check(nvgraphSetGraphStructure(handle, graph, (void*)CSC_input, NVGRAPH_CSC_32));
-    check(nvgraphAllocateVertexData(handle, graph, vertex_numsets, vertex_dimT));
-    check(nvgraphAllocateEdgeData(handle, graph, edge_numsets, &edge_dimT));
-    check(nvgraphSetEdgeData(handle, graph, (void*)weights_h, 0));
+    check(nvgraphAllocateVertexData(handle, graph, vertex_dimT.size(), vertex_dimT.data()));
+    check(nvgraphAllocateEdgeData(handle, graph, 1, &edge_dimT));
+    check(nvgraphSetEdgeData(handle, graph, (void*)graphData.weights_h.data(), 0));
+    
     // Solve
     int source_vert = 0;
     check(nvgraphSssp(handle, graph, 0, &source_vert, 0));
+    
     // Get and print result
-    check(nvgraphGetVertexData(handle, graph, (void*)sssp_1_h, 0));
+    check(nvgraphGetVertexData(handle, graph, (void*)sssp_1_h.data(), 0));
+    
+    // print shortest distances
+    for (int i = 0; i < sssp_1_h.size(); i++)
+    {
+        std::printf("%d: %f\n", i, sssp_1_h[i]);
+    }
+    
     //Clean 
-    free(sssp_1_h); free(vertex_dim);
-    free(vertex_dimT); free(CSC_input);
+    free(CSC_input);
     check(nvgraphDestroyGraphDescr(handle, graph));
     check(nvgraphDestroy(handle));
+    
     return 0;
 }
 
