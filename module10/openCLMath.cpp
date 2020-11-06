00357d4d5b7c8cdd844bde83ed577b595aa6af55
Ryan Bird <rlbird22@gmail.com>
2020-11-05 23:40:24 -0700

    Add ability for command line arguments

--- module10/openCLMath.cpp
+++ module10/openCLMath.cpp
@@ -13,12 +13,27 @@
 #include "KernelDefinitions.h"
 #include "ExampleHelperFunctions.h"
 
+#include <memory>
 #include <vector>
 
-///
-//  Constants
-//
-const int ARRAY_SIZE = 1000;
+struct CommandLineParameters
+{
+public:
+    CommandLineParameters(int argc, char** argv) {
+        for (int i = 1; i < argc; i++) {
+            const char* arg = argv[i];
+            if (strcmp(arg, "--arraysize") == 0) {
+                arraySize = atoi(argv[++i]);
+            }
+            else if (strcmp(arg, "--debug") == 0) {
+                debug = true;
+            }
+        }
+    }
+
+    bool debug = false;
+    int arraySize = 1000;
+};
 
 ///
 //  Cleanup any created OpenCL resources
@@ -59,14 +74,13 @@ void Cleanup(T item, Args... args)
     Cleanup(args...);
 }
 
-
-bool debug = false;
-
 ///
 //	main() for HelloWorld example
 //
 int main(int argc, char** argv)
 {
+    CommandLineParameters params(argc, argv);
+
     // Create an OpenCL context on first available platform
     cl_context context = nullCheck(CreateContext());
 
@@ -78,17 +92,17 @@ int main(int argc, char** argv)
     // Create memory objects that will be used as arguments to
     // kernel.  First create host memory arrays that will be
     // used to store the arguments to the kernel
-    float result[ARRAY_SIZE];
-    float a[ARRAY_SIZE];
-    float b[ARRAY_SIZE];
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    std::unique_ptr<float[]> result(new float[params.arraySize]);
+    std::unique_ptr<float[]> a(new float[params.arraySize]);
+    std::unique_ptr<float[]> b(new float[params.arraySize]);
+    for (int i = 0; i < params.arraySize; i++)
     {
         a[i] = (float)i;
         b[i] = (float)(i * 2);
     }
 
     cl_mem memObjects[3] = { 0, 0, 0 };
-    if (!CreateMemObjects(context, memObjects, a, b, ARRAY_SIZE))
+    if (!CreateMemObjects(context, memObjects, a.get(), b.get(), params.arraySize))
     {
         Cleanup(context, commandQueue, memObjects);
         return 1;
@@ -103,11 +117,13 @@ int main(int argc, char** argv)
         { powKernelFunctionName, powKernelSourceText}
     };
 
+    std::printf("Testing %d elements\n", params.arraySize);
+
     // test lambda captures local variables (context for example)
     auto testKernel = [&](const char* kernelName, const char* kernelCode) {
         std::printf("Testing: %s\n", kernelName);
 
-        if (debug)
+        if (params.debug)
             std::printf("%s\n", kernelCode);
 
         // Create OpenCL program from HelloWorld.cl kernel source
@@ -121,7 +137,7 @@ int main(int argc, char** argv)
         clCheck(clSetKernelArg(kernel, 1, sizeof(cl_mem), &memObjects[1]));
         clCheck(clSetKernelArg(kernel, 2, sizeof(cl_mem), &memObjects[2]));
 
-        size_t globalWorkSize[1] = { ARRAY_SIZE };
+        size_t globalWorkSize[1] = { params.arraySize };
         size_t localWorkSize[1] = { 1 };
 
         // Queue the kernel up for execution across the array
@@ -135,12 +151,12 @@ int main(int argc, char** argv)
 
         // Read the output buffer back to the Host
         clCheck(clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE,
-            0, ARRAY_SIZE * sizeof(float), result,
+            0, params.arraySize * sizeof(float), result.get(),
             0, NULL, NULL));
 
-        if (debug) {
+        if (params.debug) {
             // Output the result buffer
-            for (int i = 0; i < ARRAY_SIZE; i++)
+            for (int i = 0; i < params.arraySize; i++)
             {
                 std::cout << result[i] << " ";
             }
